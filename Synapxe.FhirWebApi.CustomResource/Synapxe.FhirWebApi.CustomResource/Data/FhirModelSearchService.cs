d560bdc9999bc9c9a02d1617d7c768916a549650
Bin Wei <wei.bin@synapxe.sg>
2024-10-15 22:55:33 +0800

    add search parameter, to fetch questionnaire by form type

--- Synapxe.FhirWebApi.CustomResource/Synapxe.FhirWebApi.CustomResource/Data/FhirModelSearchService.cs
+++ Synapxe.FhirWebApi.CustomResource/Synapxe.FhirWebApi.CustomResource/Data/FhirModelSearchService.cs
@@ -1,8 +1,10 @@
-﻿using Ihis.FhirEngine.Core;
+﻿using Hl7.Fhir.Model;
+using Ihis.FhirEngine.Core;
 using Ihis.FhirEngine.Core.Exceptions;
 using Ihis.FhirEngine.Core.Extensions;
 using Ihis.FhirEngine.Core.Handlers.Data;
 using Ihis.FhirEngine.Core.Search;
+using Synapxe.FhirWebApi.CustomResource.Entities;
 
 namespace Synapxe.FhirWebApi.CustomResource.Data
 {
@@ -21,13 +23,43 @@ namespace Synapxe.FhirWebApi.CustomResource.Data
 
         public IEnumerable<string> AcceptedTypes { get; } = new[]
         {
-            "Education",
-            "Appointment"
+            "AcpForm"
         };
 
         public async Task<SearchResult> SearchAsync(string resourceType, IReadOnlyList<(string, string)> queryParameters, bool isHistory, CancellationToken cancellationToken)
         {
-            throw new InvalidSearchOperationException($"SearchService '{Id}' does not support the searching the resource type '{resourceType}'.");
+            switch (resourceType)
+            {
+                case "AcpForm":
+                    return await NewMethod(queryParameters, cancellationToken).ConfigureAwait(false);
+                default:
+                    throw new InvalidSearchOperationException($"SearchService '{Id}' does not support the searching the resource type '{resourceType}'.");
+            }
+        }
+
+        private async Task<SearchResult> NewMethod(IReadOnlyList<(string, string)> queryParameters, CancellationToken cancellationToken)
+        {
+            IQueryable<AcpFormEntity> query = dbContext.Questionnaire;
+            int pageCount = 10;
+            int skip = 0;
+            var unsupported = new List<(string, string)>();
+
+            foreach (var (searchParam, modifier, value) in queryParameters.ProcessSearchParams(ref pageCount, ref skip))
+            {
+                query = searchParam switch
+                {
+                    "formType" => query.WhereAny(value, str => x => x.FormType == str, modifier),
+                    KnownQueryParameterNames.Sort => query.OrderByFhir(value, builder => builder.OrderFor(KnownQueryParameterNames.LastUpdated, e => e.LastUpdated)),
+                    _ => query.AddUnsupportedSearchParameter(unsupported, searchParam + modifier, value),
+                };
+            }
+
+            return await query.ToPagedSearchResultAsync<AcpFormEntity, AcpForm>(
+                dataMapperFactory,
+                pageCount,
+                skip,
+                unsupported,
+                cancellationToken).ConfigureAwait(false);
         }
 
         public Task<SearchResult> SearchCompartmentAsync(string compartmentType, string compartmentId, string? resourceType, IReadOnlyList<(string, string)> queryParameters, CancellationToken cancellationToken)
